90a2b8b45006e5483e5cbeb1227c5d1f93fa617f
Dentaloveit <dentaloveit@gmail.com>
2026-09-07 13:23:24 +0200

    Update [token].js

--- api/webhook/[token].js
+++ api/webhook/[token].js
@@ -1,4 +1,4 @@
-const formidable = require('formidable');
+const { formidable } = require('formidable');
 const FORMS_CONFIG = require('../../lib/formsConfig');
 const { getSubmission } = require('../../lib/jotformApi');
 const { extractPersonal, extractSimpleAnswer } = require('../../lib/jotform');
